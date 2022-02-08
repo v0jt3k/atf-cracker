0c6faf96b6b87dd257ff52439f134356c66dd22a
Tomáš Vojtek <66315153+vojtektomascz@users.noreply.github.com>
2022-02-08 20:28:06 +0100

    Upraveny alerty

--- atf-cracker.js
+++ atf-cracker.js
@@ -34,6 +34,8 @@ const ajax = {
           showAlert(3, "Uživatel nebyl nalezen v databázi!");
         } else if (response.msg.includes("heslo")) {
           showAlert(3, "Zadené heslo nesouhlasí!");
+        } else if (response.msg.includes("neexistuje")) {
+          showAlert(3, "Zadaná skupina neexistuje!");
         } else if (response.msg.includes("OK")) {
           showAlert(2);
         } else {
